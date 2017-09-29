86cc59afe24af1353f4ebd6737089ba22137e6c4
isilona <iivanov+github.com@isilona.com>
2017-09-29 21:58:10 +0300

    add Hero object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,18 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
+
+export class Hero {
+  id: number;
+  name: string;
 }
