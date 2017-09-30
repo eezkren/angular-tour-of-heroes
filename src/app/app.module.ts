bf35e8e17db67dbc290f04ef8ccc81f58816fb45
isilona <iivanov+github.com@isilona.com>
2017-09-30 19:28:54 +0300

    add dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,38 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
 import {AppComponent} from './app.component';
 import {HeroDetailComponent} from './hero-detail/hero-detail.component';
 import {HeroComponent} from './hero/hero.component';
 import {HeroService} from './hero.service';
-import {RouterModule} from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
-    HeroComponent
+    HeroComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
       {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
         path: 'heroes',
         component: HeroComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
       }
     ])
   ],
